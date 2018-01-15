8e574835facf1c16321eef901870896e29c7814d
Alexey Kopytov <akopytov@gmail.com>
2018-01-15 13:39:43 +0300

    Replace wget with curl in the RPM prebuild script.

--- packaging/rpm-oel/prebuild.sh
+++ packaging/rpm-oel/prebuild.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-cp /source/packaging/rpm-common/filter-{provides,requires}.sh /build
-wget -cP /build https://github.com/mysql/mysql-server/archive/mysql-5.6.37.tar.gz
+cd /build
+cp /source/packaging/rpm-common/filter-{provides,requires}.sh .
+curl -sSOL https://github.com/mysql/mysql-server/archive/mysql-5.6.37.tar.gz
