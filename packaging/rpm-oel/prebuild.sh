853086ec531940260ffc1c99218679d45075a99f
Alexey Kopytov <akopytov@gmail.com>
2018-01-15 11:50:51 +0300

    Fix mysql-5.6.36.tar.gz URL to download from Github rather than cdn.mysql.com.

--- packaging/rpm-oel/prebuild.sh
+++ packaging/rpm-oel/prebuild.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cp /source/packaging/rpm-common/filter-{provides,requires}.sh /build
-wget -cP /build https://cdn.mysql.com/Downloads/MySQL-5.6/mysql-5.6.37.tar.gz
+wget -cP /build https://github.com/mysql/mysql-server/archive/mysql-5.6.37.tar.gz
